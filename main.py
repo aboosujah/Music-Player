353a75fe1bdcd1ee19cb5054beaddc5e72524a75
wssfernando99 <sarindusauranga@gmail.com>
2023-06-15 21:38:09 +0530

    select music and play music function

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ class MusicPlayer:
         self.paused = False
 
         self.create_widgets()
+        
+    def select_music(self):
+        self.music_file = filedialog.askopenfilename(defaultextension=".mp3",
+        filetypes=[("MP3 Files", "*.mp3"), ("All Files", "*.*")])
+
+        if self.music_file:
+            self.song_label.config(text=f"Playing: {self.music_file}")
+
+    def play_music(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
